d249072993dc301d54c1e4665777b3df1758b2ea
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-06-02 13:11:18 -0300

    Change up version node and npm 2

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const AppMF = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/${AppMF}`));
+app.use(express.static(`${__dirname}/dist/chatBot/${AppMF}`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${AppMF}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/chatBot/${AppMF}/index.html`));
 });
  
 app.listen(process.env.PORT || 8080);
