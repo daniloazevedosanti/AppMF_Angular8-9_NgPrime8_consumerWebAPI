f03d56161af2f0b4c6e884536d35c1fd13c486dd
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-06-02 13:45:10 -0300

    Change up version node and npm 4

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/appmf'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/appmf/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+/*const express = require('express');
 const path = require('path');
 const AppMF = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/appmf`));
+app.use(express.static(`${__dirname}/dist/${AppMF}`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/appmf/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${AppMF}/index.html`));
 });
  
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);*/
