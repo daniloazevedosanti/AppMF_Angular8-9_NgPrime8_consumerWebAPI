53f7ee036da6f0a70ed456e014d240ebe04c64df
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-06-02 14:14:17 -0300

    Change up modified server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/appmf'));
+app.use(express.static(__dirname + '/dist/AppMF'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/appmf/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AppMF/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
